6194eeb0271e176fa0a2f5b3dcccdb261175ac96
Alexander DiDominic <Alexander.DiDominic@kearsarge.org>
2016-10-11 11:38:10 -0400

    Commit

--- story1.py
+++ story1.py
@@ -4,9 +4,12 @@ from input import *
 def story():
     location1 = getWord("Enter a location: ")
     temperature1 = getNumber("Enter a Number: ")
-    
+    adjective1 = getWord("Enter a adjective: ")
+    noun1 = getWord("Enter a noun: ")
     text = ""
     text += "One day I went to the " + location1
     text += ". It was like a " + temperature1
-    text += " out."    
+    text += " out."   
+    text += "Then I got very  " + adjective1
+    text += " so I went to the store to buy a " + noun1
     return text
