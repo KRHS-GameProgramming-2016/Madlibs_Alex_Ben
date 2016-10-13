d8772ed36ac964cf030d706f5a0bc8a7af4e11cb
benjamin bigelow <benjamin.bigelow@kearsarge.org>
2016-10-13 11:05:11 -0400

    jew

--- story1.py
+++ story1.py
@@ -6,10 +6,14 @@ def story():
     temperature1 = getNumber("Enter a Number: ")
     adjective1 = getWord("Enter a adjective: ")
     noun1 = getWord("Enter a noun: ")
+    adjective2 = getWord("Enter a adjective: ")
+    noun2 = getWord("Enter a noun: ")
     text = ""
     text += "One day I went to the " + location1
     text += ". It was like a " + temperature1
     text += " out."   
     text += "Then I got very  " + adjective1
     text += " so I went to the store to buy a " + noun1
+    text += " it was very " + adjective2
+    text += " . Then I continued on with my day until I found a " + noun2
     return text
