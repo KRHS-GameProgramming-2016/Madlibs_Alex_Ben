60d0c2fd4c28a925e2f4f20a1fa76d587c463288
Alexander DiDominic <Alexander.DiDominic@kearsarge.org>
2016-10-14 11:10:46 -0400

    Message

--- story1.py
+++ story1.py
@@ -12,7 +12,7 @@ def story():
     text += "One day I went to the " + location1
     text += ". It was like a " + temperature1
     text += " out."   
-    text += "Then I got very  " + adjective1
+    text += " Then I got very  " + adjective1
     text += " so I went to the store to buy a " + noun1
     text += " it was very " + adjective2
     text += " . ThenS I continued on with my day until I found a " + noun2
