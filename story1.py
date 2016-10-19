f5c577610ce24e10c97cf9432c6619e87c3df1d0
Alexander DiDominic <Alexander.DiDominic@kearsarge.org>
2016-10-19 11:46:46 -0400

    wrwewetwewetwet

--- story1.py
+++ story1.py
@@ -13,6 +13,8 @@ def story():
     adjective4 = getWord("Enter a adjective: ")
     noun4 = getWord("Enter a thing: ")
     name1= getWord("Enter a name: ")
+    noun5 = getWord("Enter a air vehical: ")
+    ocean1 = getOcean("Enter a ocean: ")
     text = ""
     text += "One day I went to the " + location1
     text += ". It was like  " + temperature1
@@ -23,9 +25,13 @@ def story():
     text += " . Then I continued on with my day until I found a " + noun2
     text += " on the street."
     text += " This made me feel very " + adjective3
-    text += " . Soon after that I ran into my favorite celberty " +noun3
+    text += " . Soon after that I ran into my favorite celberty " + noun3
     text += " . Then I went home just to find out that my mom was " + adjective4
     text += " . I just went to my room and my mom would let me leave so I got a "
     text += " to brake out of my window"
-    text += " . I climbed down and then I bumped into" +name1
+    text += " . I climbed down and then I bumped into" + name1
+    text += " He told me we have to go."
+    text += " We get into a " + noun5
+    text += " . Then we started over the " +getOcean
+    
     return text
